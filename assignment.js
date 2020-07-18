e3f296c0919216e08a0d24a1b86f666d7c66fbc7
MDSohelRana266 <sohel.aarian@gmail.com>
2020-07-18 08:18:52 -0700

    javascript assignment

--- assignment.js
+++ assignment.js
@@ -1,3 +1,4 @@
+//feetToMile//
 
 function feetToMile(feet) {
     var mile = feet * 0.000189;
@@ -5,3 +6,38 @@ function feetToMile(feet) {
 }
 var result = feetToMile(20000);
 console.log = (result);
+
+
+//woodCalculator//
+
+function woodCalculator(chair,table,bed) {
+    var threeChair = chair * 1 ;
+    var threeTable = table * 3;
+    var threeBed = bed * 5;
+    var totalWood = threeChair + threeTable + threeBed;
+    return totalWood;
+} 
+var result = woodCalculator(3,3,3);
+console.log = (result);
+
+
+//brickCalculator//
+
+function brickCalculator()
+
+
+
+
+
+//tinyFriend//
+ 
+function tinyFrind (names){
+        myFriend = names[0];
+        for( i = 0 ; i <newFriend.lenth; i++;){
+            var element = name[i];
+            myFriend = element;
+    }
+return myFriend;
+
+var result = tinyFrind(["nilima","tumpa","shamim","shuvo","sohel","kas"]);
+console.log = (result);                                       
